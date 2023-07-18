56a77b6b26a57dee3057a2e5b6e1225e5fecbec8
rilcheva <reni.ilcheva@dxc.com>
2023-07-18 22:54:21 +0300

    Add theme components

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,25 @@ import LoginPage from './scenes/loginPage';
 import ProfilePage from './scenes/profilePage';
 import { useMemo } from 'react';
 import {useSelector} from 'react-redux'
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
-
+import { CssBaseline } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { themeSettings } from './theme';
 
 function App() {
   const mode = useSelector(state => state.mode)
-  // const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
 
   return (
     <div className="app">
      <BrowserRouter>
-     {/* <ThemeProvider theme={theme}> */}
+     <ThemeProvider theme={theme}>
       <CssBaseline/>
      <Routes>
       <Route path='/' element={<LoginPage/>}></Route>
       <Route path='/home' element={<HomePage/>}></Route>
       <Route path='/profile/:userId' element={<ProfilePage/>}></Route>
      </Routes>
-     {/* </ThemeProvider> */}
+     </ThemeProvider> 
      </BrowserRouter>
     </div>
   );
